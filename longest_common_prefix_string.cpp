d1d9efb37ad5fc004c186b7c33bd3e174e25f397
Krishan <krishanduhan045@gmail.com>
2018-11-11 00:31:29 -0600

    Add files via upload

---
+++ longest_common_prefix_string.cpp
@@ -0,0 +1,50 @@
+
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+string longestCommonPrefix(vector<string>& strs) {
+        int max_len_common = 0, len_common;
+        string res;
+        string s0 = strs[0];
+        string s1;
+        int trav_length = s0.length();
+        
+        if(strs.size() == 0) return "\"\"";
+        for(int i = 1; i < strs.size(); i++) {
+            s1 = strs[i];
+            if(trav_length > s1.length()) trav_length = s1.length();
+            len_common = 0;
+            for(int j = 0; j < trav_length; j++) {
+                if(s0[j] != s1[j]) {
+                    break;
+                } else {
+                    len_common++;
+                }
+            }
+            if(len_common == 0) {
+                return "\"\"";
+            } else if(max_len_common == 0) {
+                max_len_common = len_common;
+            } else if(len_common < max_len_common) {
+                 max_len_common = len_common;   
+            }
+        }
+        res = s0.substr(0, len_common);
+        return res;
+}
+
+int main(){
+	string res;
+	vector<string> input;
+	vector<string> input1;
+	input.push_back("flower");
+        input.push_back("flow");
+        input.push_back("flight");
+
+        input1.push_back("[]");
+	res = longestCommonPrefix(input1);
+	cout << "Longest common prefix is: " << res << endl;
+	return 0;
+}
